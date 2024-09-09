01e18eaab80d260d63f2f288d53570d00fbe00b6
Ihor Dubii <ihor.dubii@unibrix.com>
2024-09-09 16:52:58 +0200

    WWT-5194: fix decimals

--- BladeSDK/src/main/java/io/bladewallet/bladesdk/Blade.kt
+++ BladeSDK/src/main/java/io/bladewallet/bladesdk/Blade.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.*
 
 @SuppressLint("StaticFieldLeak")
 object Blade {
-    private const val sdkVersion: String = "Kotlin@0.6.31"
+    private const val sdkVersion: String = "Kotlin@0.6.33"
     private var webView: WebView? = null
     private lateinit var apiKey: String
     private var visitorId: String = ""
